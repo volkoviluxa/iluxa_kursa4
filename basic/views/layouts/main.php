95ff5d9c37b33fa698c89e6867152437c46b2961
volkoviluxa <volkov101218@gmail.com>
2024-05-26 16:53:10 +0300

    начало положено

---
+++ basic/views/layouts/main.php
@@ -0,0 +1,55 @@
+<?php
+
+/** @var yii\web\View $this */
+/** @var string $content */
+
+use app\assets\AppAsset;
+use app\widgets\Alert;
+use yii\bootstrap5\Breadcrumbs;
+use yii\bootstrap5\Html;
+use yii\bootstrap5\Nav;
+use yii\bootstrap5\NavBar;
+
+AppAsset::register($this);
+
+$this->registerCsrfMetaTags();
+$this->registerMetaTag(['charset' => Yii::$app->charset], 'charset');
+$this->registerMetaTag(['name' => 'viewport', 'content' => 'width=device-width, initial-scale=1, shrink-to-fit=no']);
+$this->registerMetaTag(['name' => 'description', 'content' => $this->params['meta_description'] ?? '']);
+$this->registerMetaTag(['name' => 'keywords', 'content' => $this->params['meta_keywords'] ?? '']);
+$this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii::getAlias('@web/favicon.ico')]);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>" class="h-100">
+<head>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body class="d-flex flex-column h-100">
+<?php $this->beginBody() ?>
+
+<body>
+
+<div class="header">
+    <h1 class="marketplace-title">Название маркетплейса</h1>
+    <p class="marketplace-description">Здесь описание вашего маркетплейса, его особенности и преимущества.</p>
+</div>
+
+<li><?= Html::a('Логин',  ['/site/login']) ?></li>
+
+
+
+<div class="footer">
+    <p class="footer-text">© 2024 Маркетплейс. Все права защищены.</p>
+</div>
+
+</body>
+</html>
+
+
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
